8aa2e77718f101c95b4856101c6210029281ac28
david90farkas <davidfarkas90@gmail.com>
2021-02-16 18:33:28 +0100

    Updated mobile test

--- cypress/integration/mobileAddToFavorites.spec.js
+++ cypress/integration/mobileAddToFavorites.spec.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress" />
 
 describe("Add product to favorites", () => {
+
     before(() => {
-        cy.viewport("iphone-8")
         cy.visit("https://www.hema.nl/")
-        cy.get(".show-on-mobile button").click()
+    })
+    beforeEach(() => {
+        cy.viewport("iphone-8")
     })
     it("Select a product and add to favorites from PDP", () => {
+        cy.get(".show-on-mobile button").click()
         cy.contains(".wrap", "menu").click()
         .contains(".mobile-category-menu", "kind").click({force: true})
         .contains("tops, shirts en blouses").click({force: true})
@@ -18,19 +21,24 @@ describe("Add product to favorites", () => {
         //cy.get(".icon-heart").should("have.css", "color", "rgb(218,45,42)")
     })
     it("Remove product from favorites PDP", () => {
-        cy.viewport("iphone-8")
         cy.get("div.btns-wrap i.icon-heart").click()
         cy.get("li.favorites-link span").should("have.class", "favorites-number hidden")
     })
-    // it("Select a product and add to favorites from PLP", () =>{
-    //     cy.get(".categories-wrap button").trigger('mouseover')
-    //     cy.get("div[class='category-menu js-gtm-mainnav'] li a").contains('dames').trigger('mouseover').get('.inner ul li a').contains('nachthemden').click({force: true})
-    //     cy.get('.capture h1').contains('nachthemden')
-    //     cy.selectFavorites(3)
-    //     cy.get("span.favorites-number").should("have.text", "(1)1")
-    // })
-    // it("Remove product from favorites PLP", () => {
-    //     cy.selectFavorites(3)
-    //     cy.get(".favorites-wrap").trigger("mouseover").contains(".empty-text", "wat vind jij leuk?").should("be.visible")
-    // })
+    it("Select a product and add to favorites from PLP", () => {
+
+        cy.contains(".wrap", "menu").click()
+        .contains(".mobile-category-menu", "dames").click({force: true})
+        .contains("nachthemden").click({force: true})
+        cy.get('.capture h1').contains('nachthemden')
+        cy.selectFavorites(3).then(($el)=>{
+            cy.wrap($el).should("have.class", "add-to-favorites js-favorites js-favorites-heart added red-heart")
+            cy.get(".mobile-favorites-added").should('be.visible')
+        })
+        
+    })
+    it("Remove product from favorites PLP", () => {
+        cy.selectFavorites(3).then(($el)=>{
+            cy.wrap($el).should("have.class", "add-to-favorites js-favorites js-favorites-heart")
+        })
+    })
 })
