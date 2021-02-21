c536f4f458ba0fa0426fa625cfb527f1d22e3dc5
david90farkas <davidfarkas90@gmail.com>
2021-02-21 20:12:22 +0100

    Added new test

--- cypress/integration/addToFavorites.spec.js
+++ cypress/integration/addToFavorites.spec.js
@@ -30,4 +30,26 @@ describe("Add product to favorites", () => {
         cy.selectFavorites(3)
         cy.get(".favorites-wrap").trigger("mouseover").contains(".empty-text", "wat vind jij leuk?").should("be.visible")
     })
+
+    it("Navigate to QVP and add product to favorites", () => {
+        cy.selectCategory("wonen en slapen", "kandelaar")
+        cy.get(".capture h1").contains("kandelaar")
+        cy.quickViewProduct(3)
+        cy.get(".btns-wrap .js-favorites-heart").click().should("have.class", "js-favorites-heart added red-heart")
+    })
+
+    it("Remove product from favorites QVP", () => {
+        cy.get(".btns-wrap .js-favorites-heart").click().should("not.have.class", "js-favorites-heart added red-heart")
+    })
+
+    it.only("Add product to favorites and remove it from favorites dropdown", () =>{
+        cy.get(".categories-wrap button").trigger('mouseover')
+        cy.selectCategory("dames", "badmode")
+        cy.get('.filter-wrapper h1').contains('badmode')
+        cy.selectFavorites(2)
+        cy.selectFavorites(3)
+        cy.get("span.favorites-number").should("have.text", "(2)2")
+        cy.removeFromFavorites("dames bikinibroekje - animal")
+        cy.get(".favorites-wrap").trigger("mouseover").find(".product-info .title").should("not.have.text", "dames bikinibroekje - animal")
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -34,6 +34,9 @@ Cypress.Commands.add("genEmail", () => {
     var email = 'd.far' + num + '@gmail.com';
     return email
 })
+Cypress.Commands.add("quickViewProduct", (product) => {
+    cy.xpath(".//div[@class='product-container'][" + product + "]//div//button").click({force: true})
+})
 Cypress.Commands.add("selectProduct", (product) => {
     cy.xpath(".//div[@class='product-container'][" + product + "]//div/a[@class='js-product-link']").click({force: true})
 })
@@ -43,4 +46,8 @@ Cypress.Commands.add("selectFavorites", (product) => {
 Cypress.Commands.add("selectCategory", (mainCatgeory, subCategory) => {
     cy.get(".categories-wrap").trigger("mouseover").find("[class='category-tree clearfix']").contains(mainCatgeory).
     trigger("mouseover").parent().find("[class='group has-children']").contains(subCategory).click({force: true})
+})
+
+Cypress.Commands.add("removeFromFavorites", (product) => {
+    cy.get(".favorites-wrap").trigger("mouseover").find(".product-item").contains(product).parent().find(".remove-basket-product").click({force: true})
 })
